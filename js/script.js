039a1aad67fdf3d232ea3a88bd40cd961d0e3f25
m-litovka <michael.litovka@gmail.com>
2023-01-25 17:38:11 +0200

    Lab task 2

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@
 let numberOfFilms = prompt("How many films have you seen?");
 
 console.log(numberOfFilms);
+
+let personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
