c3c64e2c9a1c4fe1da79d27d32baaf91985ba38a
m-litovka <michael.litovka@gmail.com>
2023-01-25 17:57:07 +0200

    Lab task 3

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 //nothing to see here
 
-let numberOfFilms = prompt("How many films have you seen?");
+const numberOfFilms = prompt("How many films have you seen?");
 
 console.log(numberOfFilms);
 
-let personalMoviesDB = {
+let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+
+let movieName = prompt("What's a movie you've seen recently?", "Movie name");
+let movieRating = prompt("How much do you rate it out of 10? (Decimals allowed):", "(7.2 ex.)");
+personalMovieDB.movies[movieName] = movieRating;
+
+movieName = prompt("What's a movie you've seen recently?", "Movie name");
+movieRating = prompt("How much do you rate it out of 10? (Decimals allowed):", "(7.2 ex.)");
+personalMovieDB.movies[movieName] = movieRating;
+
+
+console.log(personalMovieDB.movies);
