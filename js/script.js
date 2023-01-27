1bc2219b2879d540b986076455f726729f3a38f5
m-litovka <michael.litovka@gmail.com>
2023-01-27 17:58:26 +0200

    Logical operators' lesson

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
 //nothing to see here
 
-const numberOfFilms = prompt("How many films have you seen?");
+// const numberOfFilms = prompt("How many films have you seen?");
 
-console.log(numberOfFilms);
+// console.log(numberOfFilms);
 
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-let a = prompt("What's a movie you've seen recently?", "Movie name");
-let movieRating = prompt("How much do you rate it out of 10? (Decimals allowed):", "(7.2 ex.)");
-personalMovieDB.movies[a] = movieRating;
+// let a = prompt("What's a movie you've seen recently?", "Movie name");
+// let movieRating = prompt("How much do you rate it out of 10? (Decimals allowed):", "(7.2 ex.)");
+// personalMovieDB.movies[a] = movieRating;
 
-let b = prompt("What's a movie you've seen recently?", "Movie name");
-movieRating = prompt("How much do you rate it out of 10? (Decimals allowed):", "(7.2 ex.)");
-personalMovieDB.movies[b] = movieRating;
+// let b = prompt("What's a movie you've seen recently?", "Movie name");
+// movieRating = prompt("How much do you rate it out of 10? (Decimals allowed):", "(7.2 ex.)");
+// personalMovieDB.movies[b] = movieRating;
 
 
-console.log(personalMovieDB.movies);
+// console.log(personalMovieDB.movies);
 
-(personalMovieDB.movies[a] === "9") ? console.log("Nice choice!") : console.log("Joey disapproves");
+// (personalMovieDB.movies[a] === "9") ? console.log("Nice choice!") : console.log("Joey disapproves");
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
